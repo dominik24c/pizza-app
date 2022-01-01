2a0cb2101a2fac3f6a8d139b28590002a25b65bd
dominik24c <dominik24c@gmail.com>
2022-01-01 20:19:26 +0100

    update manifest.json - remove icons, update OrderMessage Component - dont show this message when user switches between different routes

--- src/components/pizza/OrderMessage.js
+++ src/components/pizza/OrderMessage.js
@@ -2,26 +2,26 @@ import {useDispatch, useSelector} from 'react-redux';
 import { useEffect } from 'react';
 import ideliveryManImg from '../../img/deliveryman.png';
 import { resetMessages } from '../../store/pizza/cart-reducer';
+import { useLocation } from 'react-router-dom';
 
 import './OrderMessage.css';
 
 const OrderMessage = () => {
     const successMessage = useSelector(state => state.cart.successMessage);
-    
+    const location = useLocation();
     const dispatch =  useDispatch();
 
     useEffect(()=>{
         if(successMessage){
-        const timer = setTimeout(()=>{
-            dispatch(resetMessages());
-        },5000);
-
-        return ()=>clearTimeout(timer);
+            const timer = setTimeout(()=>{
+                dispatch(resetMessages());
+            },5000);
+    
+            return ()=>clearTimeout(timer);
         }
     },[successMessage, dispatch]);
 
-
-    if (!successMessage){
+    if(!successMessage || location.pathname!=='/'){
         return null;
     }
 
