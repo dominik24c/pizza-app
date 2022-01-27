6ca35c607113b47f1b494153415e72baf8c56704
dominik24c <dominik24c@gmail.com>
2022-01-27 23:07:02 +0100

    fix OrderMessage component - remove component when user switch to other route

--- src/components/pizza/OrderMessage.js
+++ src/components/pizza/OrderMessage.js
@@ -1,34 +1,46 @@
 import {useDispatch, useSelector} from 'react-redux';
-import { useEffect } from 'react';
-import ideliveryManImg from '../../img/deliveryman.png';
-import { resetMessages } from '../../store/pizza/cart-reducer';
-import { useLocation } from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import deliveryManImg from '../../img/deliveryman.png';
+import {resetMessages} from '../../store/pizza/cart-reducer';
+import {useLocation} from 'react-router-dom';
 
 import './OrderMessage.css';
 
 const OrderMessage = () => {
     const successMessage = useSelector(state => state.cart.successMessage);
     const location = useLocation();
-    const dispatch =  useDispatch();
+    const dispatch = useDispatch();
+    const [timer, setTimer] = useState(null);
+    const [isMessageOnMenuPage,setIsMessageOnMenuPage] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         if(successMessage){
-            const timer = setTimeout(()=>{
-                dispatch(resetMessages());
-            },5000);
-    
-            return ()=>clearTimeout(timer);
+            const t = setTimeout(() => {
+            setIsMessageOnMenuPage(false);
+            dispatch(resetMessages());
+            }, 5000);
+            setTimer(t);
+            setIsMessageOnMenuPage(true);
         }
-    },[successMessage, dispatch]);
+    }, [successMessage, dispatch]);
 
-    if(!successMessage || location.pathname!=='/'){
+    if(isMessageOnMenuPage && location.pathname !== '/'){
+        setIsMessageOnMenuPage(false);
+        if(timer){
+            clearTimeout(timer);
+            setTimer(null);
+            dispatch(resetMessages());
+        }
+    }
+
+    if (!successMessage || location.pathname !== '/') {
         return null;
     }
 
     return (
         <div className='order-message'>
             <h3>{successMessage.message}</h3>
-            <img src={ideliveryManImg} alt='delivery man'/>
+            <img src={deliveryManImg} alt='delivery man'/>
         </div>
     );
 }
